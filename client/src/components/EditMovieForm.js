cb71f377ceb6b567fa9c1b9639923b8f931d193a
Jack Henry <jackson.lewis.henry@gmail.com>
2021-03-24 15:26:55 -0400

    done with deleteMovie

--- client/src/components/EditMovieForm.js
+++ client/src/components/EditMovieForm.js
@@ -5,8 +5,6 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const EditMovieForm = (props) => {
-	const { push } = useHistory();
-
 	const [movie, setMovie] = useState({
 		title:"",
 		director: "",
@@ -14,6 +12,18 @@ const EditMovieForm = (props) => {
 		metascore: 0,
 		description: ""
 	});
+	const { id } = useParams();
+	const { push } = useHistory();
+
+	useEffect(()=>{
+		axios.get(`http://localhost:5000/api/movies/${id}`)
+		.then(res=>{
+			setMovie(res.data);
+		})
+		.catch(err=>{
+			console.log(err.response);
+		});
+	}, []);
 	
 	const handleChange = (e) => {
         setMovie({
@@ -24,7 +34,17 @@ const EditMovieForm = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-	}
+		axios.put(`http://localhost:5000/api/movies/${id}`, movie)
+		.then((res)=>{
+			// console.log(res)
+			props.setMovies(res.data);
+
+			push(`/movies/${id}`);
+		})
+		.catch(err=>{
+			console.log(err);
+		})
+	};
 	
 	const { title, director, genre, metascore, description } = movie;
 
